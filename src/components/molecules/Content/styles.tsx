d9eac4c8d90dd9428d9f22d3c7d2387127373230
David Ruiz <davidl-ruizc@outlook.com>
2021-06-26 16:45:12 -0500

    set atom design

---
+++ src/components/molecules/Content/styles.tsx
@@ -0,0 +1,6 @@
+import { BACKGROUND_CARD } from 'assets/styles/colors';
+import styled from 'styled-components';
+
+export const ContentWrapper = styled.div`
+  background-color: ${BACKGROUND_CARD};
+`;
---
+++ src/components/organisms/Home/index.tsx
@@ -0,0 +1,8 @@
+import Content from 'components/molecules/Content';
+import * as React from 'react';
+
+const HomeList: React.FC = () => {
+  return <Content>embeces</Content>;
+};
+
+export default HomeList;
