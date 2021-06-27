b32e4a0f391c727639000a7a9e603aa1b5cc80e3
David Ruiz <davidl-ruizc@outlook.com>
2021-06-27 16:49:43 -0500

    render ingredients from reducer

--- src/components/molecules/CreatePizzaForm/Step1/index.tsx
+++ src/components/molecules/CreatePizzaForm/Step1/index.tsx
@@ -1,6 +1,8 @@
 import Ingredient from 'components/atoms/Ingredient';
 import * as React from 'react';
+import { useSelector } from 'react-redux';
 import { Button } from 'reactstrap';
+import { RootState } from 'states';
 import {
   NextWrapper,
   PriceLabel,
@@ -15,6 +17,8 @@ interface Step1Props {
 }
 
 const Step1: React.FC<Step1Props> = ({ onNextStep }) => {
+  const ingredients = useSelector((state: RootState) => state.IngredientsReducer.ingredients);
+
   return (
     <StepContainer>
       <StepImage
@@ -23,22 +27,13 @@ const Step1: React.FC<Step1Props> = ({ onNextStep }) => {
         src="https://images.unsplash.com/photo-1571066811602-716837d681de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=831&q=80"
         alt="Card image cap"
       />
-      <Ingredient ingredient="Pepperoni" price={4500} />
-      <Ingredient ingredient="Pollo" price={5000} />
-      <Ingredient ingredient="Queso" price={3000} />
-      <Ingredient ingredient="Jamón" price={3000} />
-      <Ingredient ingredient="Tocineta" price={3000} />
-      <Ingredient ingredient="Chorizo" price={3000} />
-      <Ingredient ingredient="Salchicha" price={3000} />
-      <Ingredient ingredient="Jalapeños" price={3000} />
-      <Ingredient ingredient="Aceitunas" price={3000} />
-      <Ingredient ingredient="Tomates" price={3000} />
-      <Ingredient ingredient="Chile" price={3000} />
-      <Ingredient ingredient="Mostaza" price={3000} />
-      <Ingredient ingredient="Piña" price={3000} />
-      <Ingredient ingredient="Maíz" price={3000} />
-      <Ingredient ingredient="Huevo" price={3000} />
-      <Ingredient ingredient="Masa" price={10000} disabled={true} />
+      {ingredients.map((ingredient) => (
+        <Ingredient
+          ingredient={ingredient.ingredient}
+          price={ingredient.price}
+          disabled={ingredient.disabled}
+        />
+      ))}
       <PriceWrapper>
         <PriceLabel>Precio Total:</PriceLabel>
         <PriceValue>$100000</PriceValue>
