55c4732f9037f004b612286bbad0045dd19e0ec4
David Ruiz <davidl-ruizc@outlook.com>
2021-06-27 17:52:11 -0500

    set final price action

--- src/actions/redux/ingredientsAction.ts
+++ src/actions/redux/ingredientsAction.ts
@@ -1,6 +1,16 @@
-import { IUpdateIngredients, UPDATE_INGREDIENTS } from 'states/reducers/ingredients.reducer';
+import {
+  ISetFinalPrice,
+  IUpdateIngredients,
+  SET_FINAL_PRICE,
+  UPDATE_INGREDIENTS,
+} from 'states/reducers/ingredients.reducer';
 
 export const UpdateIngredients = (ingredient: IIngredientsSelection): IUpdateIngredients => ({
   type: UPDATE_INGREDIENTS,
   payload: ingredient,
+});
+
+export const SetFinalPrice = (price: number): ISetFinalPrice => ({
+  type: SET_FINAL_PRICE,
+  payload: price,
 });
