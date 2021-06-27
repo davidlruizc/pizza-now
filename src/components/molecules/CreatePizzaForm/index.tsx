b5ff2f52da0c9508673603cbeec23f2b221c22a8
David Ruiz <davidl-ruizc@outlook.com>
2021-06-27 12:52:12 -0500

    setup wizard steps

--- src/components/molecules/CreatePizzaForm/index.tsx
+++ src/components/molecules/CreatePizzaForm/index.tsx
@@ -2,11 +2,30 @@ import * as React from 'react';
 import { Container } from 'reactstrap';
 import Step1 from './Step1';
 import Step2 from './Step2';
+import StepWizard from 'react-step-wizard';
 
 const CreatePizzaForm: React.FC = () => {
+  const [wizardInstance, setWizardInstance] = React.useState<any>();
+
+  const setNextStep = () => {
+    if (wizardInstance) {
+      wizardInstance.nextStep();
+      window.scrollTo(0, 0);
+    }
+  };
+
+  const prevStep = () => {
+    if (wizardInstance) {
+      wizardInstance.previousStep();
+    }
+  };
+
   return (
     <Container>
-      <Step2 />
+      <StepWizard instance={setWizardInstance}>
+        <Step1 onNextStep={setNextStep} />
+        <Step2 onPrevStep={prevStep} />
+      </StepWizard>
     </Container>
   );
 };
