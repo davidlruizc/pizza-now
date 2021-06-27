151f78afecd22422ca56c4fb2af5f9438b804788
David Ruiz <davidl-ruizc@outlook.com>
2021-06-27 16:46:48 -0500

    combine reducers

--- src/interfaces/ingredients.d.ts
+++ src/interfaces/ingredients.d.ts
@@ -1,6 +1,11 @@
 declare interface IIngredientsSelection {
+  id: number;
   ingredient: string;
   price: number;
   quantity: number;
   disabled?: boolean;
+}
+
+declare interface IIngredientsSelectionReducer {
+  ingredients: IIngredientsSelection[];
 }
