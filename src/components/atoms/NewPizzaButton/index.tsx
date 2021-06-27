0652235e6eeab4106ef764f38f2447d6e8b7f688
David Ruiz <davidl-ruizc@outlook.com>
2021-06-26 19:57:55 -0500

    changes on colors

--- src/components/atoms/NewPizzaButton/index.tsx
+++ src/components/atoms/NewPizzaButton/index.tsx
@@ -7,7 +7,7 @@ interface NewPizzaButtonProps {
 
 const NewPizzaButton: React.FC<NewPizzaButtonProps> = ({ onClick }) => {
   return (
-    <ButtonStyled color="primary" size="lg" onClick={onClick}>
+    <ButtonStyled size="lg" onClick={onClick}>
       <i className="fa fa-plus" />
     </ButtonStyled>
   );
--- src/components/atoms/NewPizzaButton/styles.ts
+++ src/components/atoms/NewPizzaButton/styles.ts
@@ -1,3 +1,4 @@
+import { BACKGROUND_COLOR, PRIMARY_COLOR } from 'assets/styles/colors';
 import { Button } from 'reactstrap';
 import styled from 'styled-components';
 
@@ -5,4 +6,6 @@ export const ButtonStyled = styled(Button)`
   border-radius: 50%;
   width: 50px;
   height: 50px;
+  background-color: ${PRIMARY_COLOR};
+  color: ${BACKGROUND_COLOR};
 `;
