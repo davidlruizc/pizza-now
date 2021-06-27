cdad936111af9f809fefd3534ccc2d27a5883119
David Ruiz <davidl-ruizc@outlook.com>
2021-06-27 17:52:42 -0500

    add, remove and show final price calculation

--- src/components/molecules/CreatePizzaForm/Step1/index.tsx
+++ src/components/molecules/CreatePizzaForm/Step1/index.tsx
@@ -1,8 +1,10 @@
+import { SetFinalPrice, UpdateIngredients } from 'actions/redux/ingredientsAction';
 import Ingredient from 'components/atoms/Ingredient';
 import * as React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'reactstrap';
 import { RootState } from 'states';
+import { FormatCurrency } from 'utils';
 import {
   NextWrapper,
   PriceLabel,
@@ -18,6 +20,32 @@ interface Step1Props {
 
 const Step1: React.FC<Step1Props> = ({ onNextStep }) => {
   const ingredients = useSelector((state: RootState) => state.IngredientsReducer.ingredients);
+  const finalPriceSelector = useSelector((state: RootState) => state.IngredientsReducer.finalPrice);
+  const dispatch = useDispatch();
+
+  const addIngredient = (ingredient: IIngredientsSelection) => {
+    let increaseQuantity = { ...ingredient, quantity: ingredient.quantity + 1 };
+
+    dispatch(UpdateIngredients(increaseQuantity));
+  };
+
+  const removeIngredient = (ingredient: IIngredientsSelection) => {
+    if (ingredient.quantity !== 0) {
+      let increaseQuantity = { ...ingredient, quantity: ingredient.quantity - 1 };
+
+      dispatch(UpdateIngredients(increaseQuantity));
+    }
+  };
+
+  React.useEffect(() => {
+    let total = 0;
+    for (let i = 0; i < ingredients.length; i++) {
+      total += ingredients[i].quantity * ingredients[i].price;
+    }
+
+    const finalPrice = Math.round(total * 100) / 100;
+    dispatch(SetFinalPrice(finalPrice));
+  }, [ingredients, dispatch]);
 
   return (
     <StepContainer>
@@ -29,14 +57,18 @@ const Step1: React.FC<Step1Props> = ({ onNextStep }) => {
       />
       {ingredients.map((ingredient) => (
         <Ingredient
+          key={ingredient.id}
           ingredient={ingredient.ingredient}
           price={ingredient.price}
           disabled={ingredient.disabled}
+          quantity={ingredient.quantity}
+          addIngredient={() => addIngredient(ingredient)}
+          removeIngredient={() => removeIngredient(ingredient)}
         />
       ))}
       <PriceWrapper>
         <PriceLabel>Precio Total:</PriceLabel>
-        <PriceValue>$100000</PriceValue>
+        <PriceValue>{FormatCurrency(finalPriceSelector)}</PriceValue>
       </PriceWrapper>
       <NextWrapper>
         <Button color="primary" size="lg" block onClick={onNextStep}>
