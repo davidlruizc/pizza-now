ae5266499dfc1cd6147f24b0e32032aefae5427d
David Ruiz <davidl-ruizc@outlook.com>
2021-06-26 21:42:22 -0500

    test big card

---
+++ src/components/molecules/BigCard/BigCard.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import BigCard from './index';
+import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+
+describe('<BigCard />', () => {
+  test('render big card', () => {
+    const data = {
+      title: 'Crea tu pizza',
+      subtitle: 'Elige los sabores que más te gusten y arma una pizza a tu medida',
+    };
+
+    const component = render(
+      <BigCard title={data.title} subtitle={data.subtitle}>
+        Pizza Now
+      </BigCard>
+    );
+
+    component.getByText('Crea tu pizza');
+    component.getByText('Elige los sabores que más te gusten y arma una pizza a tu medida');
+
+    const tree = renderer
+      .create(
+        <BigCard title={data.title} subtitle={data.subtitle}>
+          Pizza Now
+        </BigCard>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
--- src/components/molecules/BigCard/index.tsx
+++ src/components/molecules/BigCard/index.tsx
@@ -5,20 +5,10 @@ import { BodyCard, CardContainer, CardRow, SubTitle } from './styles';
 interface BigCardProps {
   children: React.ReactNode;
   title: string;
-  padding?: string;
-  height?: string;
   subtitle?: string;
-  actionButton?: React.ReactNode;
 }
 
-const BigCard: React.FC<BigCardProps> = ({
-  children,
-  title,
-  padding,
-  height,
-  subtitle,
-  actionButton,
-}) => {
+const BigCard: React.FC<BigCardProps> = ({ children, title, subtitle }) => {
   return (
     <CardContainer className="shadow bg-secondary">
       <div className="bg-white border-0 card-header">
@@ -27,16 +17,9 @@ const BigCard: React.FC<BigCardProps> = ({
             <h2 className={'mb-0'}>{title}</h2>
             {subtitle && <SubTitle>{subtitle}</SubTitle>}
           </Col>
-          {actionButton && (
-            <Col xs={4} md={4} sm={4}>
-              {actionButton}
-            </Col>
-          )}
         </CardRow>
       </div>
-      <BodyCard padding={padding} height={height}>
-        {children}
-      </BodyCard>
+      <BodyCard>{children}</BodyCard>
     </CardContainer>
   );
 };
