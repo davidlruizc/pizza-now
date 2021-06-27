25d7053b129d88408c7c4cb4dcc8a1181b6b6397
David Ruiz <davidl-ruizc@outlook.com>
2021-06-26 21:19:01 -0500

    Big card component

--- src/components/organisms/CreatePizza/index.tsx
+++ src/components/organisms/CreatePizza/index.tsx
@@ -1,8 +1,10 @@
+import BigCard from 'components/molecules/BigCard';
 import Content from 'components/molecules/Content';
 import Header from 'components/molecules/Header';
 import { NavigationUrls } from 'constants/navigationURLS';
 import * as React from 'react';
 import { useHistory } from 'react-router-dom';
+import { Container } from 'reactstrap';
 
 const CreatePizza: React.FC = () => {
   const history = useHistory();
@@ -14,6 +16,14 @@ const CreatePizza: React.FC = () => {
   return (
     <Content>
       <Header onNavigate={onListPizzas} close={true} />
+      <Container>
+        <BigCard
+          title="Crea tu pizza"
+          subtitle="Elige los sabores que más te gusten y arma una pizza a tu medida"
+        >
+          asdfs
+        </BigCard>
+      </Container>
     </Content>
   );
 };
