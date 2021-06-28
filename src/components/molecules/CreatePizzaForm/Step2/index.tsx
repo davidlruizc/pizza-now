bbc91c19e3b2050c7f749a42ef9ba9db7d594762
David Ruiz <davidl-ruizc@outlook.com>
2021-06-27 19:16:56 -0500

    control hook form

--- src/components/molecules/CreatePizzaForm/Step2/index.tsx
+++ src/components/molecules/CreatePizzaForm/Step2/index.tsx
@@ -1,33 +1,113 @@
 import * as React from 'react';
-import { Button, Form, FormGroup, Input, Label, Row } from 'reactstrap';
+import { Controller, useForm } from 'react-hook-form';
+import { useSelector } from 'react-redux';
+import { Button, Form, FormFeedback, FormGroup, Input, Label, Row } from 'reactstrap';
+import { RootState } from 'states';
+import { FormatCurrency, hookFormValidation } from 'utils';
 import { ButtonCol, PriceLabel, PriceValue, PriceWrapper, SubmitWrapper } from './styles';
 
 interface Step2Props {
   onPrevStep: () => void;
 }
 
+interface Step2Form {
+  name: string;
+  pizzaName: string;
+  phone: string;
+}
+
 const Step2: React.FC<Step2Props> = ({ onPrevStep }) => {
+  const finalPriceSelector = useSelector((state: RootState) => state.IngredientsReducer.finalPrice);
+
+  const defaultValues: Step2Form = {
+    name: '',
+    pizzaName: '',
+    phone: '',
+  };
+  const getDefaultFormValues = (): Step2Form => {
+    return defaultValues;
+  };
+
+  const {
+    formState: { errors },
+    handleSubmit,
+    control,
+    reset,
+  } = useForm<Step2Form>({ ...hookFormValidation, defaultValues: getDefaultFormValues() });
+
+  const submitPizzaCreation = (info: Step2Form) => {
+    console.log(info);
+  };
+
   return (
     <Form>
       <FormGroup>
         <Label for="exampleEmail">Nombre</Label>
-        <Input type="text" name="email" placeholder="Ingresa tu nombre" />
+        <Controller
+          control={control}
+          name="name"
+          render={({ field: { onChange, value } }) => (
+            <React.Fragment>
+              <Input
+                type="text"
+                name="name"
+                placeholder="Ingresa tu nombre"
+                onChange={onChange}
+                value={value}
+                invalid={errors.name ? true : false}
+              />
+              {errors.name && <FormFeedback>Este campo es obligatorio</FormFeedback>}
+            </React.Fragment>
+          )}
+          rules={{ required: true }}
+        />
       </FormGroup>
       <FormGroup>
         <Label for="exampleNumber">Teléfono</Label>
-        <Input type="number" name="number" placeholder="Ingresa tu número de teléfono" />
+        <Controller
+          control={control}
+          name="phone"
+          render={({ field: { onChange, value } }) => (
+            <React.Fragment>
+              <Input
+                type="number"
+                name="number"
+                placeholder="Ingresa tu número de teléfono"
+                onChange={onChange}
+                value={value}
+                invalid={errors.phone ? true : false}
+                min={0}
+              />
+              {errors.phone && <FormFeedback>Este campo es obligatorio</FormFeedback>}
+            </React.Fragment>
+          )}
+          rules={{ required: true }}
+        />
       </FormGroup>
       <FormGroup>
         <Label for="exampleEmail">Nombra tu pizza</Label>
-        <Input
-          type="text"
-          name="email"
-          placeholder="Dale un nombre a tu pizza: Ej, Pizza carnivora"
+        <Controller
+          control={control}
+          name="pizzaName"
+          render={({ field: { onChange, value } }) => (
+            <React.Fragment>
+              <Input
+                type="text"
+                name="pizzaName"
+                placeholder="Dale un nombre a tu pizza: Ej, Pizza carnivora"
+                onChange={onChange}
+                value={value}
+                invalid={errors.pizzaName ? true : false}
+              />
+              {errors.pizzaName && <FormFeedback>Este campo es obligatorio</FormFeedback>}
+            </React.Fragment>
+          )}
+          rules={{ required: true }}
         />
       </FormGroup>
       <PriceWrapper>
         <PriceLabel>Precio Total:</PriceLabel>
-        <PriceValue>$100000</PriceValue>
+        <PriceValue>{FormatCurrency(finalPriceSelector)}</PriceValue>
       </PriceWrapper>
       <SubmitWrapper>
         <Row>
@@ -37,7 +117,7 @@ const Step2: React.FC<Step2Props> = ({ onPrevStep }) => {
             </Button>
           </ButtonCol>
           <ButtonCol sm="6">
-            <Button color="primary" size="lg" block>
+            <Button color="primary" size="lg" block onClick={handleSubmit(submitPizzaCreation)}>
               Ordenar
             </Button>
           </ButtonCol>
--- src/components/molecules/CreatePizzaForm/index.tsx
+++ src/components/molecules/CreatePizzaForm/index.tsx
@@ -3,23 +3,9 @@ import { Container } from 'reactstrap';
 import Step1 from './Step1';
 import Step2 from './Step2';
 import StepWizard from 'react-step-wizard';
-import { useForm } from 'react-hook-form';
-import { hookFormValidation } from 'utils';
-
-interface Step1Form extends IIngredients {
-  price: number;
-}
 
 const CreatePizzaForm: React.FC = () => {
   const [wizardInstance, setWizardInstance] = React.useState<any>();
-
-  const {
-    formState: { errors: errorsStep1 },
-    handleSubmit: submitStep1,
-    setValue: setValueStep1,
-    control: controlStep1,
-    reset: resetStep1,
-  } = useForm<Step1Form>(hookFormValidation);
 
   const setNextStep = () => {
     if (wizardInstance) {
