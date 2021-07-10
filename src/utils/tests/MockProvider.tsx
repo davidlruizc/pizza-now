db129263cc1e4d90e841fcfbd2a2a95c57768fbc
David Ruiz <davidl-ruizc@outlook.com>
2021-07-10 17:44:45 -0500

    add toas provider

--- src/utils/tests/MockProvider.tsx
+++ src/utils/tests/MockProvider.tsx
@@ -4,12 +4,15 @@ import { createStore } from 'redux';
 import { MemoryRouter } from 'react-router-dom';
 import rootReducer from 'states';
 import { mount } from 'enzyme';
+import { ToastProvider } from 'react-toast-notifications';
 
 const store = createStore(rootReducer);
 
 export const MockProvider = (path: string, component: React.ReactElement) =>
   mount(
     <MemoryRouter initialEntries={[path]}>
-      <Provider store={store}>{component}</Provider>
+      <ToastProvider autoDismiss autoDismissTimeout={5000}>
+        <Provider store={store}>{component}</Provider>
+      </ToastProvider>
     </MemoryRouter>
   );
