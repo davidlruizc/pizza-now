97bf298832d8112e15ba93fdf44bbfb13aecfd0e
David Ruiz <davidl-ruizc@outlook.com>
2021-07-10 18:19:44 -0500

    not found test

--- src/pages/NotFound/__test__/index.test.tsx
+++ src/pages/NotFound/__test__/index.test.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import { MockProvider } from 'utils/tests/MockProvider';
 import NotFound from '../index';
+import { ContentLabel } from '../styles';
 
 describe('<NotFound />', () => {
+  const component = MockProvider('/random', <NotFound />);
+
   test('render content', () => {
-    const component = MockProvider('/random', <NotFound />);
     expect(component.length).toEqual(1);
+  });
+
+  test('verify main content', () => {
+    expect(component.find(ContentLabel).text()).toEqual('404 Página no encontrada');
   });
 });
